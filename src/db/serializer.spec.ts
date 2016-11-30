3c5caecbb4182d2c9066ad965235571a2c15154d
Thomas Pink <thomas.pink@netural.com>
2016-11-30 22:50:10 +0100

    added license comments

--- src/db/serializer.spec.ts
+++ src/db/serializer.spec.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright (c) 2016 Thomas Pink
+ *
+ * Use of this source code is governed by the MIT-style license that can be
+ * found in the LICENSE file at https://github.com/thomaspink/ng-storyblok/blob/master/LICENSE
+ */
+
 import { SBDefaultSerializer } from './serializer';
 import { SBComponent, SBStory } from './model';
 
