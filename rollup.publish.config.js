31bd96087abded433f995eead016106abf205b73
Thomas Pink <thomas.pink@netural.com>
2016-12-05 16:12:16 +0100

    removed rollup publish

--- rollup.publish.config.js
+++
@@ -1,9 +0,0 @@
-import globals from './rollup-globals';
-
-export default {
-  entry: 'dist/index.js',
-  dest: 'dist/bundles/storyblok.umd.js',
-  format: 'umd',
-  moduleName: 'storyblok',
-  globals
-}
